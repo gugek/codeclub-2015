ed74f1ae00a53425ec1249be2b496892d7371395
Sean Chen <slc.chen@gmail.com>
2015-05-03 22:19:08 -0400

    marc files

--- hello_marc.py
+++ hello_marc.py
@@ -9,22 +9,18 @@ import argparse
 
 
 def main(arguments):
-    reader = pymarc.MARCReader(arguments.filename, force_utf8=True)
-    record = reader.next()  # returns a record object
+    marc_file = open(arguments.filename, 'rb')
+    reader = pymarc.MARCReader(marc_file, force_utf8=True)
+    record = next(reader)  # returns a record object
     print("Hello World!")
     print(record)  # print the MARC-text representation of the record
     return record
 
 
-    # alternate reading using iteration
-    """
-    for record in reader:  # python idiom for looping through or iterating
-        print(record)
-    """
-
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="MARC file to open")
+    parser.add_argument("-f", "--field", help="fieldname to print")
     arguments = parser.parse_args()
     return arguments
 
---
+++ marc_csv_demo.py
@@ -0,0 +1,71 @@
+"""
+Extract data from a MARC file and output to CSV.
+
+Use with Python 3. Unicode handling expected.
+"""
+import pymarc
+import csv
+import argparse
+import sys
+
+
+def main(args):
+    csv_writer = make_csv_writer(args.output, args.fields)
+    csv_writer.writeheader()
+    for marc_file in args.marc_files:
+        marc_reader = pymarc.MARCReader(marc_file)
+        for record in marc_reader:
+            row = {}  # create a dictionary row
+            for fieldnames in make_fieldnames(args.fields, csv=False):
+                fields = record.get_fields(*fieldnames)
+                data = []
+                for field in fields:
+                    data.append(field.format_field())
+                row["+".join(fieldnames)] = " | ".join(data)
+            csv_writer.writerow(row)
+    args.output.close()
+
+
+def make_fieldnames(fields, csv=True):
+    """Return fieldnames from the args, Some encapsulation here"""
+    fieldnames = []
+    for field_name in fields.split(','):
+        # handle 001, 002, 003
+        elements = []
+        for element in field_name.strip().split('+'):
+            elements.append(element.strip())
+        fieldnames.append(elements)
+    if csv:
+        return ["+".join(field_name) for field_name in fieldnames]
+    else:
+        return fieldnames
+
+
+def make_csv_writer(output, fields):
+    """Returns a CSV DictWriter"""
+    fieldnames = make_fieldnames(fields)
+    csv_writer = csv.DictWriter(output, fieldnames=fieldnames)
+    return csv_writer
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    # MUST read MARC as binary
+    parser.add_argument("marc_files", help="MARC file(s) to open", nargs="+",
+                        type=argparse.FileType('rb'))
+    parser.add_argument("-o", "--output", default=sys.stdout,
+                        type=argparse.FileType('w'),
+                        help="output file: default stdout")
+    parser.add_argument("-f", "--fields",
+                        help="fields to extract: comma and plus separated " +
+                             "list, example: 100+110+111+130,245,250")
+    arguments = parser.parse_args()
+    return arguments
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main(args)
+
+
+
